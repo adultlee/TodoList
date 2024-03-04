8ff4901345e548c05ba59b28878297bb506db05f
이성인 <devlsi1228@gmail.com>
2024-03-04 14:33:01 +0900

    feat: 최초 렌더링시 content type 체크

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ async function fetchTodosApiCall() {
 	const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos/`, {
 		cache: "no-store",
 	});
+	const contentTypeHeaderValue = data.headers.get("Content-Type");
 
+	if (contentTypeHeaderValue?.includes("text/html")) {
+		return null;
+	}
 	return data.json();
 }
 
@@ -16,7 +20,7 @@ export default async function Home() {
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center ">
 				<h1 className={title()}>Adultlee Todo</h1>
-				<TodosTable todos={data} />
+				{data ? <TodosTable todos={data} /> : "데이터가 연결되지 않았습니다."}
 			</div>
 		</section>
 	);
